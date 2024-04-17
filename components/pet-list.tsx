522d5bdb354e76f96d1481ad0b4bf5d298565f90
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-04-16 17:40:03 -0700

    Displaying pets from server

--- components/pet-list.tsx
+++ components/pet-list.tsx
@@ -1,9 +1,22 @@
-export default function PetList() {
+import Image from "next/image";
+
+export default function PetList({ pets }) {
   return (
     <ul className="bg-white border-b border-black/[0.08]">
-      <li>
-        <button>Benjamin</button>
-      </li>
+      {pets.map((pet) => (
+        <li key={pet.id}>
+          <button className="flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+            <Image
+              src={pet.imageUrl}
+              width={45}
+              height={45}
+              alt="Pet image"
+              className="w-[45px] h-[45px] rounded-full object-cover"
+            />
+            <p className="font-semibold">{pet.name}</p>
+          </button>
+        </li>
+      ))}
     </ul>
   );
 }
