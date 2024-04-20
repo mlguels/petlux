c780fa23add294a4a8ca733a16030ed4a4a1af0a
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-04-19 17:54:30 -0700

    Add Selected pet styling

--- components/pet-list.tsx
+++ components/pet-list.tsx
@@ -1,17 +1,21 @@
 "use client";
 
-import { usePetContext } from "@/lib/hooks";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { usePetContext } from "@/lib/hooks";
 
 export default function PetList() {
-  const { pets, handleChangeSelectedId } = usePetContext();
+  const { pets, handleChangeSelectedId, selectedPet } = usePetContext();
   return (
     <ul className="bg-white border-b border-black/[0.08]">
       {pets.map((pet) => (
         <li key={pet.id}>
           <button
             onClick={() => handleChangeSelectedId(pet.id)}
-            className="flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition"
+            className={cn(
+              "flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition",
+              { "bg-[#EFF1F2]": selectedPet === pet.id }
+            )}
           >
             <Image
               src={pet.imageUrl}
