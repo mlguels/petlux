d2337760ddc017dc67b40f385275e73364d29520
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-04-23 16:53:20 -0700

    Add Buttons to pet details

--- components/pet-details.tsx
+++ components/pet-details.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 
 import { usePetContext } from "@/lib/hooks";
 import { Pet } from "@/lib/types";
+import PetButton from "./pet-button";
 
 export default function PetDetails() {
   const { selectedPet } = usePetContext();
@@ -49,6 +50,11 @@ function TopBar({ pet }: Props) {
       />
 
       <h2 className="text-3xl font-semibold leading-7 ml-5">{pet.name}</h2>
+
+      <div className="ml-auto space-x-2">
+        <PetButton actionType="edit">Edit</PetButton>
+        <PetButton actionType="checkout">Checkout</PetButton>
+      </div>
     </div>
   );
 }
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 ",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-full text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 ",
   {
     variants: {
       variant: {
@@ -13,15 +13,15 @@ const buttonVariants = cva(
         destructive: "bg-red-500 text-zinc-50 shadow-sm hover:bg-red-500/90 ",
         outline:
           "border border-zinc-200 bg-white shadow-sm hover:bg-zinc-100 hover:text-zinc-900 ",
-        secondary: "bg-zinc-100 text-zinc-900 shadow-sm hover:bg-zinc-100/80 ",
+        secondary: "bg-zinc-200 text-zinc-900 shadow-sm hover:bg-zinc-300 ",
         ghost: "hover:bg-zinc-100 hover:text-zinc-900 ",
         link: "text-zinc-900 underline-offset-4 hover:underline ",
       },
       size: {
         default: "h-10 px-6 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
+        sm: "h-8 rounded-lg px-3 text-xs",
+        lg: "h-10 rounded-lg px-8",
+        icon: "h-14 w-14",
       },
     },
     defaultVariants: {
