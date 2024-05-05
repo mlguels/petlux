64d4f9bed15445a6bf3c56bd68d17334ed06eb6c
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-05-05 13:23:23 -0700

    Replaced fetch with prisma client

--- app/(app)/app/layout.tsx
+++ app/(app)/app/layout.tsx
@@ -1,25 +1,17 @@
-import { Pet } from "@/lib/types";
-
 import AppFooter from "@/components/app-footer";
 import AppHeader from "@/components/app-header";
 import BackgroundPattern from "@/components/background-pattern";
 
 import PetContextProvider from "@/context/pet-context-provider";
 import SearchContextProvider from "@/context/search-context-provider";
+import prisma from "@/lib/db";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const response = await fetch(
-    "https://bytegrad.com/course-assets/projects/petsoft/api/pets"
-  );
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch pets");
-  }
-  const data: Pet[] = await response.json();
+  const pets = await prisma.pet.findMany();
 
   return (
     <>
@@ -29,7 +21,7 @@ export default async function Layout({
         <AppHeader />
 
         <SearchContextProvider>
-          <PetContextProvider data={data}>{children}</PetContextProvider>
+          <PetContextProvider data={pets}>{children}</PetContextProvider>
         </SearchContextProvider>
 
         <AppFooter />
