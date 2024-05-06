b7261943f1f951cdcccc9c76ce990800b15ef542
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-05-06 07:18:35 -0700

    Add server action for form input

--- actions/actions.ts
+++ actions/actions.ts
@@ -2,8 +2,14 @@
 
 import prisma from "@/lib/db";
 
-export async function addPet(pet) {
+export async function addPet(formdata) {
   await prisma.pet.create({
-    data: pet,
+    data: {
+      name: formdata.get("name"),
+      ownerName: formdata.get("ownerName"),
+      imageUrl: formdata.get("imageUrl"),
+      age: +formdata.get("age"),
+      notes: formdata.get("notes"),
+    },
   });
 }
