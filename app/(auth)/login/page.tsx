c7319ad7b55d0a83d1b64b3994b09175b96df848
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-05-15 15:02:17 -0700

    Init signup / login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,20 @@
+import Link from "next/link";
+
+import AuthForm from "@/components/auth-form";
+import H1 from "@/components/h1";
+
 export default function Page() {
-  return <div>Login</div>;
+  return (
+    <main>
+      <H1 className="mb-5 text-center">Log In</H1>
+      <AuthForm />
+
+      <p className="mt-6 text-sm text-zinc-500">
+        Don&apos;t have an account?{" "}
+        <Link href="/signup" className="font-medium">
+          Sign up
+        </Link>
+      </p>
+    </main>
+  );
 }
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,3 +1,20 @@
+import Link from "next/link";
+
+import AuthForm from "@/components/auth-form";
+import H1 from "@/components/h1";
+
 export default function Page() {
-  return <div>SignUpPage</div>;
+  return (
+    <main>
+      <H1 className="mb-5 text-center">Sign up</H1>
+      <AuthForm />
+
+      <p className="mt-6 text-sm text-zinc-500">
+        Don&apos;t have an account?{" "}
+        <Link href="/login" className="font-medium">
+          Log In
+        </Link>
+      </p>
+    </main>
+  );
 }
