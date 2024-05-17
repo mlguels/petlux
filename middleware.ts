dccf50670c0a9fd590780cabfad826927c6665d8
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-05-17 11:31:30 -0700

    Updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,7 @@
-import { NextResponse } from "next/server";
+import { auth } from "./lib/auth";
 
-export function middleware(request: Request) {
-  console.log(request.url);
-  return NextResponse.next();
-}
+export default auth;
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
-  ],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
