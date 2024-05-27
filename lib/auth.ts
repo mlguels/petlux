9cf7e6f619821d37c213cfd1002d5552e2f0d699
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-05-27 15:42:44 -0700

    Add auth login

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,44 @@
 import NextAuth, { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import prisma from "./db";
+import bcrypt from "bcryptjs";
 
 const config = {
   pages: {
     signIn: "/login",
   },
-  providers: [],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        // runs on login
+        const { email, password } = credentials;
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email,
+          },
+        });
+        if (!user) {
+          console.log("User not found");
+          return null;
+        }
+
+        const passwordsMatch = await bcrypt.compare(
+          password,
+          user.hashedPassword
+        );
+
+        if (!passwordsMatch) {
+          console.log("Invalid credentials");
+        }
+
+        return user;
+      },
+    }),
+  ],
   callbacks: {
-    authorized: ({ request }) => {
+    authorized: ({ auth, request }) => {
+      // runs on every request with middleware
       const isTryingToAccessApp = request.nextUrl.pathname.includes("/app");
 
       if (isTryingToAccessApp) {
